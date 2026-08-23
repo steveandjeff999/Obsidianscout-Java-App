0ebb58a51894a3ec8399c3cfc4136697f947f7a1
steveandjeff999 <150098465+steveandjeff999@users.noreply.github.com>
2026-08-22 22:50:28 -0500

    update

---
+++ windows/permission_handler_stub.cpp
@@ -0,0 +1,4 @@
+#include "permission_handler_windows/permission_handler_windows_plugin.h"
+extern "C" {
+FLUTTER_PLUGIN_EXPORT void PermissionHandlerWindowsPluginRegisterWithRegistrar(FlutterDesktopPluginRegistrarRef registrar) {}
+}
---
+++ windows/permission_handler_stub_include/permission_handler_windows/permission_handler_windows_plugin.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <flutter_plugin_registrar.h>
+#ifdef FLUTTER_PLUGIN_IMPL
+#define FLUTTER_PLUGIN_EXPORT __declspec(dllexport)
+#else
+#define FLUTTER_PLUGIN_EXPORT __declspec(dllimport)
+#endif
+#if defined(__cplusplus)
+extern "C" {
+#endif
+FLUTTER_PLUGIN_EXPORT void PermissionHandlerWindowsPluginRegisterWithRegistrar(FlutterDesktopPluginRegistrarRef registrar);
+#if defined(__cplusplus)
+}
+#endif
